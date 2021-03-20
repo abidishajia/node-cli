88a7e514a5173698f9faad2f0aa44fb504c8fdb0
Shajia Abidi <sabidi@pewresearch.org>
2021-03-20 16:11:27 -0700

    First Version

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+console.log(`
+    Shajia Abidi
+
+    I'm a front end web developer, and I've been creating interactive data-driven websites 
+    professionally for over four years now. I also enjoy writing, and I have been published 
+    in various publications like Smashing Magazine, Kite, Twilio, LogRocket, 
+    The Counter, The Tempest, Pew Research Center among others. 
+
+    Twitter: https://twitter.com/abidishajia/
+    Github: https://github.com/abidishajia
+    LinkedIn: http://linkedin.com/in/abidishajia
+
+`);
